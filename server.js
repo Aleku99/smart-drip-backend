74c423b682a08f05917a726a51627d5dfe591e07
Alexandru Loghin <loghinalex19@gmail.com>
2021-12-06 01:18:34 +0200

    Added firebase signup

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+var uuid = require("uuid");
+var admin = require("firebase-admin");
+
 const app = express();
 const port = 3001;
 
@@ -9,11 +12,23 @@ const { default: axios } = require("axios");
 app.use(cors()); // Use this after the variable declaration
 app.use(express.json());
 
+var serviceAccount = require("./smart-drip-b2119-firebase-adminsdk-p406q-6660750f24.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://smart-drip-b2119-default-rtdb.europe-west1.firebasedatabase.app",
+});
+var db = admin.database();
+var ref = db.ref();
+ref.on("value", function (snapshot) {
+  console.log(snapshot.val());
+});
+
 app.get("/", (req, res) => {
   res.send(`<h1 style="text-align: center">smart-drip-backend</h1>`);
 });
 app.post("/login", (req, res) => {
-  res.send("Got a POST request from login screen");
+  console.log(req.body);
 });
 app.post("/signup", (req, res) => {
   console.log(req.body);
@@ -34,12 +49,9 @@ app.post("/signup", (req, res) => {
       email: email,
       hash: hash,
     };
-    axios.post(
-      "https://smart-drip-b2119-default-rtdb.europe-west1.firebasedatabase.app/.json",
-      entry
-    );
+    ref.child(uuid.v1()).set(entry);
   });
-  res.send("Signup successfull");
+  res.send("Signup req successfull");
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
