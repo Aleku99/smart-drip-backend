e2124ffc8e7e3b9a8be348cea087b76464690a00
Alex Loghin <loghinalex19@gmail.com>
2022-01-10 23:49:02 +0200

    Added GPIO led blink depending on sent configuration(fix)

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 var uuid = require("uuid");
 var admin = require("firebase-admin");
-const Gpio = require("onoff").Gpio;
-const led = new Gpio(2, "out");
+var Gpio = require('onoff').Gpio; 
+var LED = new Gpio(17, 'out'); 
+var intervalID = 0; //TODO: fix intervalID reference error
+
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
+
+
 
 const app = express();
 const port = 3001;
@@ -70,13 +81,31 @@ app.post("/signup", (req, res) => {
     res.status(200).send(entry);
   });
 });
+
+app.post("/change_config", async (req,res)=>{
+    const chosen_config = await req.body.mode;
+    if(chosen_config == 0){
+      if(intervalID){
+        clearInterval(intervalID);
+      }
+      intervalID = setInterval(blinkLED, 250);
+    }
+    else if(chosen_config == 1){
+      if(intervalID){
+        clearInterval(intervalID);
+      }
+      intervalID = setInterval(blinkLED, 1000);
+    }
+    else{
+      if(intervalID){
+        clearInterval(intervalID);
+      }
+      LED.writeSync(0);
+    }
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-for (let i = 0; i < 1000000; i++) {
-  setTimeout(function () {
-    led.writeSync(true);
-  }, 2000);
-  led.writeSync(false);
-}
+
