28149854bfaa7a886a1724d22dae8b0b54267641
Alexandru Loghin <loghinalex19@gmail.com>
2022-06-11 17:06:06 +0300

    refactor: remove unecessary imports

--- firebase.js
+++ firebase.js
@@ -1,13 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  child,
-  push,
-  update,
-} from "firebase/database";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBgMUU94MW75z9gIXMG_rWTuJixKgZRQeE",
